dac75a360301c33b755f794f2ed0acbb599abe12
Phuc Dang <phuchoang2710@gmail.com>
2019-08-02 23:11:06 +0700

    Update config

---
+++ src/screens/Main/MainContainer.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+@connect(null, null)
+class MainContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default MainContainer;
